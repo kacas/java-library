add460fb7cfad1f964d1aa7e3690a9432d931384
kacastrajnic@gmail.com <kacastrajnic@gmail.com>
2017-03-08 23:25:15 +0100

    minor

--- src/test/java/com/library/service/internal/JPABookServiceTest.java
+++ src/test/java/com/library/service/internal/JPABookServiceTest.java
@@ -14,9 +14,7 @@ import static org.junit.Assert.assertEquals;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Matchers.any;
 import static org.mockito.Matchers.anyString;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -124,7 +122,6 @@ public class JPABookServiceTest {
         bookService.deleteBook(bookToDeleteId);
 
         verify(bookRepository).delete(bookToDeleteId);
-        verify(bookRepository, Mockito.never()).delete(UUID.randomUUID());
-        verify(bookRepository, Mockito.times(1)).delete(bookToDeleteId);
+        verifyNoMoreInteractions(bookRepository);
     }
 }
