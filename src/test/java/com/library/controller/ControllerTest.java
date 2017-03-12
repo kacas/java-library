e431c40f213dd9d48d468b3ee243de124dca308b
smartinov <stefan.martinov@gmail.com>
2017-03-12 18:55:16 +0100

    Change controller test to abstract, since it's used for testing only

--- src/test/java/com/library/controller/ControllerTest.java
+++ src/test/java/com/library/controller/ControllerTest.java
@@ -1,7 +1,6 @@
 package com.library.controller;
 
 import org.junit.Before;
-import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -14,13 +13,14 @@ import org.springframework.web.context.WebApplicationContext;
 
 /**
  * Created by Katarina on 3/6/2017.
+ * <p>
+ * Base controller test that will be inherited by all tests
  */
-
 @RunWith(SpringJUnit4ClassRunner.class)
 @Transactional
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @ActiveProfiles("test")
-public class ControllerTest {
+public abstract class ControllerTest {
 
     @Autowired
     protected WebApplicationContext webApplicationContext;
