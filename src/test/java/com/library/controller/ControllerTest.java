aacd43fd60ea054089cd7e5e6f0cc9240569ad45
smartinov <stefan.martinov@gmail.com>
2017-03-12 18:54:13 +0100

    Remove unused annotation, replace with @SpringBootTest

--- src/test/java/com/library/controller/ControllerTest.java
+++ src/test/java/com/library/controller/ControllerTest.java
@@ -1,28 +1,24 @@
 package com.library.controller;
 
-import com.library.LibraryApplicationTests;
 import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.context.WebApplicationContext;
-import org.springframework.boot.test.SpringApplicationConfiguration;
 
 /**
  * Created by Katarina on 3/6/2017.
  */
 
 @RunWith(SpringJUnit4ClassRunner.class)
-//@ContextConfiguration(classes = LibraryApplicationTests.class)
-@SpringApplicationConfiguration(classes = LibraryApplicationTests.class)
-@WebAppConfiguration
 @Transactional
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @ActiveProfiles("test")
 public class ControllerTest {
 
@@ -35,5 +31,6 @@ public class ControllerTest {
     public void setUp() {
         mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
     }
+
 
 }
